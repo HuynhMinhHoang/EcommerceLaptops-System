3af4d113fb156b41f4c93ce0100ed55e9798fd17
HuynhMinhHoang <hminhhoangdev@gmail.com>
2024-07-17 01:27:49 +0700

    add carousel product list

--- client/src/components/Home/ContentHome/LaptopList.js
+++ client/src/components/Home/ContentHome/LaptopList.js
@@ -1,52 +1,26 @@
 import React from "react";
 import "./LaptopList.scss";
 import laptop1 from "../../../assets/laptop1.png";
-import { FaTruck } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
-
-const ProductCard = ({ product }) => {
-  return (
-    <div className="product-card">
-      <div className="product-image">
-        <img src={product.image} alt={product.name} />
-      </div>
-      <div className="product-info">
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-description">
-          <span className="cpu">{product.cpu}</span>
-          <span className="gpu">{product.gpu}</span>
-          <span className="ram">{product.ram}</span>
-          <span className="storage">{product.storage}</span>
-          <span className="display">{product.display}</span>
-        </p>
-        <div className="product-price">
-          <span className="original-price">{product.originalPrice}</span>
-          <span className="discount-price">
-            {product.discountPrice}{" "}
-            <span className="discount-percentage">
-              {product.discountPercentage}
-            </span>
-          </span>
-        </div>
-        <div className="product-rating">
-          <span className="rating-score">
-            {product.ratingScore}
-            <FaStar
-              style={{
-                fontSize: "13px",
-                marginLeft: "3px",
-                marginBottom: "4px",
-              }}
-            />
-          </span>
-          <span className="rating-reviews">{product.ratingReviews}</span>
-        </div>
-      </div>
-    </div>
-  );
-};
+import { FaTruck, FaStar } from "react-icons/fa";
+// import Carousel from "react-elastic-carousel";
+// import Slider from "react-slick";
+import CustomCarousel from "../../../utils/CustomCarousel";
 
 const LaptopList = () => {
+  // const settings = {
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 500,
+  //   slidesToShow: 5,
+  //   slidesToScroll: 5,
+  // };
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
   const products = [
     {
       name: "Laptop gaming Acer Nitro 5 Tiger AN515 58 GB598J",
@@ -132,21 +106,8 @@ const LaptopList = () => {
       ratingScore: "0.0",
       ratingReviews: "(0 đánh giá)",
     },
-    {
-      name: "Laptop gaming Acer Nitro V ANVI15 51 588AN",
-      image: laptop1,
-      cpu: "i7-13620H",
-      gpu: "RTX 2050",
-      ram: "16 GB",
-      storage: "512 GB",
-      display: "15.6 inch FHD @ 144 Hz",
-      originalPrice: "₫25.990.000đ",
-      discountPrice: "₫23.990.000đ",
-      discountPercentage: "-8%",
-      ratingScore: "0.0",
-      ratingReviews: "(0 đánh giá)",
-    },
   ];
+
   return (
     <div className="product-container">
       <div className="tilte-product">
@@ -163,13 +124,54 @@ const LaptopList = () => {
           />
           Miễn phí giao hàng
         </span>
-
         <p>Xem tất cả</p>
       </div>
       <div className="product-list">
-        {products.map((product, index) => (
-          <ProductCard key={index} product={product} />
-        ))}
+        <CustomCarousel breakPoints={breakPoints}>
+          {products.map((product, index) => (
+            <div key={index} className="product-card">
+              <div className="product-image">
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className="product-info">
+                <h3 className="product-name">{product.name}</h3>
+                <p className="product-description">
+                  <span className="cpu">{product.cpu}</span>
+                  <span className="gpu">{product.gpu}</span>
+                  <span className="ram">{product.ram}</span>
+                  <span className="storage">{product.storage}</span>
+                  <span className="display">{product.display}</span>
+                </p>
+                <div className="product-price">
+                  <span className="original-price">
+                    {product.originalPrice}
+                  </span>
+                  <span className="discount-price">
+                    {product.discountPrice}{" "}
+                    <span className="discount-percentage">
+                      {product.discountPercentage}
+                    </span>
+                  </span>
+                </div>
+                <div className="product-rating">
+                  <span className="rating-score">
+                    {product.ratingScore}
+                    <FaStar
+                      style={{
+                        fontSize: "13px",
+                        marginLeft: "3px",
+                        marginBottom: "4px",
+                      }}
+                    />
+                  </span>
+                  <span className="rating-reviews">
+                    {product.ratingReviews}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </CustomCarousel>
       </div>
     </div>
   );
